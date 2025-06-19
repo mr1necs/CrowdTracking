dd56e6a60d7516dd6949657acc56df8798c29acb
Maks Mr1necs <46063054+mr1necs@users.noreply.github.com>
2025-06-19 16:21:47 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,36 +3,47 @@ from argparse import ArgumentParser
 
 def get_arguments() -> dict[str, str]:
     """
-    Parse command-line arguments.
+    Parse command-line arguments for the application.
 
     Returns:
-        A dictionary of arguments.
+        dict[str, str]: Dictionary of parsed command-line arguments.
     """
     parser = ArgumentParser()
 
     parser.add_argument(
-        "-i", "--input",
+        "-i",
+        "--input",
         type=str,
         default="crowd.mp4",
-        #required=True,
-        help="Path to input video (e.g. crowd.mp4)"
+        # required=True,
+        help="Path to input video (e.g. crowd.mp4)",
     )
     parser.add_argument(
-        "-o", "--output",
+        "-o",
+        "--output",
         type=str,
         default="output.mp4",
-        help="Path to save annotated video"
+        help="Path to save annotated video",
     )
     parser.add_argument(
-        "-m", "--model",
+        "-m",
+        "--model",
         type=str,
         default="yolo11n.pt",
-        help="Path to load YOLO model (or YOLO model name (e.g. yolo11n.pt))"
+        help="Path to load YOLO model (or YOLO model name (e.g. yolo11n.pt))",
     )
     parser.add_argument(
-        "-d", "--device",
+        "-d",
+        "--device",
         type=str,
         default="cpu",
-        help="Device to use: 'mps', 'cuda', or 'cpu'"
+        help="Device to use: 'mps', 'cuda', or 'cpu'",
+    )
+    parser.add_argument(
+        "-s",
+        "--show",
+        action="store_true",
+        default=False,
+        help="Show video on screen (default: False)",
     )
     return vars(parser.parse_args())
